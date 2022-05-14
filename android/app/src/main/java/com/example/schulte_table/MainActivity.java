0efbbb657b82122c06ef505cd2ff9f51b671c4dd
Guan Peng <1718090900@qq.com>
2022-05-14 19:03:20 +0800

    recreate project to fit flutter version 3

--- android/app/src/main/java/com/example/schulte_table/MainActivity.java
+++ android/app/src/main/java/com/example/schulte_table/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.schulte_table;
+package live.goan.schulte_table;
 
 import io.flutter.embedding.android.FlutterActivity;
 
---
+++ android/app/src/main/kotlin/com/example/schulte_table_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package live.goan.schulte_table_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
